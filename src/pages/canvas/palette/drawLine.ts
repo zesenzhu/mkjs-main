e0e4d484adb158b32af6f33c69ba6b5fa6e4bce0
zhuzesen <2362039373@qq.com>
2021-11-21 21:10:20 +0800

    'feat:增加画笔功能'

--- src/pages/canvas/palette/drawLine.ts
+++ src/pages/canvas/palette/drawLine.ts
@@ -1,32 +1,78 @@
 import canvasUtils from "../../../utils/canvasUtils";
 
 type PreXYType = {
-  x: number | null;
-  y: number | null;
+    x: number;
+    y: number;
+    end?: boolean
 };
-const preXY: PreXYType = { x: null, y: null };
-const drawLine = (event: any, ctx: CanvasRenderingContext2D) => {
-  const { x, y, isPressed } = event;
-  // 结束时候回位
-  if (!isPressed) {
-    preXY.x = null;
-    preXY.y = null;
-  }
-  // 开始时候记录
-  if (preXY.x === null && preXY.y === null) {
-    preXY.x = x;
-    preXY.y = y;
-    return;
-  }
-  ctx.beginPath();
-  ctx.strokeStyle = "pink";
-  ctx.quadraticCurveTo(preXY.x as number, preXY.y as number, x, y);
-  ctx.stroke();
-  ctx.closePath();
+const preXY: PreXYType = { x: -1, y: -1 };
+const pointList: PreXYType[] = []
+let headDrive = 0
+const drawLine = (event: any, canvas: HTMLCanvasElement) => {
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
-  // 记录上一次的位置
-  preXY.x = x;
-  preXY.y = y;
+    const { x, y, isPressed } = event;
+    // console.log(event)
+    if (x === null || y === null || !isPressed) {
+        preXY.x = -1
+        preXY.y = -1
+        return
+    }
+    // console.log(x, y)
+
+    pointList.push({
+        x, y,
+    })
+
+    const draw = (ctx: CanvasRenderingContext2D) => {
+        // if (headDrive < 30) {
+        //     headDrive++
+        //     window.requestAnimationFrame(draw.bind(this, ctx))
+
+        //     return
+        // }
+        // headDrive = 0
+        if (pointList.length <= 0) {
+            return
+        }
+        const [{ x, y, }] = pointList.splice(0, 1);
+        // ctx.save();
+        // ctx.clearRect(0, 0, canvas.width, canvas.height)
+        if ((preXY.x === -1 && preXY.y === -1)) {
+            const radius = ctx.lineWidth / 2
+            const fillStyle = ctx.fillStyle;
+            ctx.fillStyle = ctx.strokeStyle;
+            ctx.lineJoin = "round";
+            ctx.lineCap = "round";
+            ctx.beginPath()
+            ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+            ctx.closePath()
+            ctx.fill()
+            ctx.fillStyle = fillStyle
+        } else {
+            ctx.beginPath()
+
+            ctx.moveTo(preXY.x, preXY.y);
+            ctx.lineTo(x, y);
+            ctx.stroke();
+            ctx.closePath()
+
+        }
+
+        preXY.x = x
+        preXY.y = y
+
+
+        // ctx.restore();
+        // window.requestAnimationFrame(draw.bind(this, ctx))
+
+    }
+    draw(ctx)
+
+
+
 };
+
+
 
 export default drawLine;
--- src/pages/canvas/palette/index.tsx
+++ src/pages/canvas/palette/index.tsx
@@ -1,76 +1,96 @@
+import { Button, InputNumber } from "antd";
 import React, { useEffect, useRef, useState } from "react";
 import canvasUtils from "../../../utils/canvasUtils";
 import drawLine from "./drawLine";
+import './index.css'
 const { captureTouch, captureClick } = canvasUtils;
 const Canvas: React.FC = () => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+    const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const moveRef = useRef<any>({});
-  const ctxRef = useRef<CanvasRenderingContext2D | null>();
-  const [drawType, setDrawType] = useState("line");
-  useEffect(() => {
-    if (canvasRef.current) {
-      const ctx = (ctxRef.current = canvasRef.current.getContext("2d"));
-      captureTouch(canvasRef.current, moveEvent);
-      captureClick(canvasRef.current, moveEvent);
+    const moveRef = useRef<any>({});
+    const ctxRef = useRef<CanvasRenderingContext2D | null>();
 
-      console.log(ctx);
-      // draw();
+
+    const [drawType, setDrawType] = useState("line");
+
+    const resize = (ctx: CanvasRenderingContext2D) => {
+        if (canvasRef.current) {
+
+            console.log(ctx.lineWidth)
+            canvasRef.current.height = canvasRef.current?.clientHeight / window.devicePixelRatio
+            canvasRef.current.width = canvasRef.current?.clientWidth / window.devicePixelRatio
+            ctx.strokeStyle = "red";
+            // ctx.lineWidth = 2;
+            ctx.lineJoin = "round";
+            ctx.lineCap = "round";
+        }
     }
+    useEffect(() => {
+        if (canvasRef.current) {
+            const ctx = (ctxRef.current = canvasRef.current.getContext("2d")) as CanvasRenderingContext2D;
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+            resize(ctx)
+            captureTouch(canvasRef.current, moveEvent);
+            captureClick(canvasRef.current, moveEvent);
+            window.addEventListener('resize', () => {
+                resize(ctx)
+            })
+            // draw();
+        }
 
-  useEffect(() => {
-    let removeTouch = () => {};
-    let removeClick = () => {};
-    if (canvasRef.current) {
-      removeTouch = captureTouch(canvasRef.current, moveEvent);
-      removeClick = captureClick(canvasRef.current, moveEvent);
-    }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-    return () => {
-      removeTouch && removeTouch();
-      removeClick && removeClick();
+    useEffect(() => {
+        let removeTouch = () => { };
+        let removeClick = () => { };
+        if (canvasRef.current) {
+            removeTouch = captureTouch(canvasRef.current, moveEvent);
+            removeClick = captureClick(canvasRef.current, moveEvent);
+        }
+
+        return () => {
+            removeTouch && removeTouch();
+            removeClick && removeClick();
+        };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [drawType]);
+    const moveEvent = (type: string, event: any) => {
+        moveRef.current = event;
+        switch (type) {
+            case 'mouseup':
+            case 'touchend':
+                ctxRef.current?.closePath();
+                break;
+            case 'touchstart':
+            case 'mousedown':
+                ctxRef.current?.beginPath();
+                break;
+            default:
+
+
+        }
+        if (drawType === "line") {
+            ctxRef.current && drawLine(event, canvasRef.current as HTMLCanvasElement);
+        }
     };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [drawType]);
-  const moveEvent = (type: string, event: any) => {
-    moveRef.current = event;
-    if (drawType === "line") {
-      ctxRef.current && drawLine(event, ctxRef.current);
-    }
-  };
 
-  // const draw: any = () => {
-  //   drawPoint();
-  //   // window.requestAnimationFrame(draw);
-  // };
-  // const drawLine = () => {
-  //   if (ctxRef.current) {
-  //     ctxRef.current.strokeStyle = "pink";
-  //     ctxRef.current.moveTo(moveRef.current.x, moveRef.current.y);
-  //     ctxRef.current.lineTo(100, 100);
-  //     ctxRef.current.stroke();
-  //   }
-  // };
-  // const drawPoint = (radius: number = 1, color: string = "rgba(0,0,0 )") => {
-  //   if (ctxRef.current) {
-  //     ctxRef.current?.beginPath();
-  //     ctxRef.current?.arc(
-  //       moveRef.current.x - radius,
-  //       moveRef.current.y - radius,
-  //       radius,
-  //       0,
-  //       Math.PI * 2
-  //     );
-  //     ctxRef.current?.closePath();
-  //     ctxRef.current.fillStyle = color;
-  //     ctxRef.current?.fill();
-  //   }
-  // };
-  return (
-    <canvas ref={canvasRef} style={{ height: "100%", width: "100%" }}></canvas>
-  );
+
+
+
+    const clearPalette = () => {
+        ctxRef.current?.clearRect(0, 0, canvasRef.current?.width as number, canvasRef.current?.height as number)
+    }
+    return (
+        <div style={{ height: '100%', width: '100%' }} >
+            <Button className='handle' type="primary" onClick={clearPalette}   >清屏</Button>
+            <InputNumber addonBefore={'线条宽度'} min={1} max={50} onChange={(value: any) => {
+                if (ctxRef.current) ctxRef.current.lineWidth = value
+            }} defaultValue={2} />
+            <canvas ref={canvasRef}
+
+                style={{ marginTop: 10, border: '1px #000 solid', height: '100%', width: '100%' }}></canvas>
+        </div>
+    );
 };
 export default Canvas;
