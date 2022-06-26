f798c94ebf1c20d3797f6fb2be04e995a244de8f
zhuzesen <xizhi.zhu@tuya.com>
2022-06-26 23:58:52 +0800

    feat:修改主应用环境变量注入

--- src/micro_app.ts
+++ src/micro_app.ts
@@ -1,7 +1,17 @@
+// 动态注入,区分生产环境和开发环境,BUILD_HOST这个在start.js设置的,所以在运行yarn start的时候要带上--buildHost=${ip}
+const buildHost =
+  (process.env.NODE_ENV === "production" && process.env.BUILD_HOST) ||
+  "localhost";
+console.log(
+  "micro_app",
+  process.env.BUILD_HOST,
+  process.env.npm_config_buildHost
+);
+
 const microApps = [
   {
     name: "micro_react",
-    entry: "//localhost:9999/",
+    entry: `//${buildHost}:9999/`,
     activeRule: "/micro_react",
     container: "#subapp-viewport", // 子应用挂载的div
     props: {
