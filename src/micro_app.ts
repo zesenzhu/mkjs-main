348132d41856cd3e7fff911cadc24819268cb9d6
zhuzesen <xizhi.zhu@tuya.com>
2022-07-10 22:05:29 +0800

    feat:改造框架

--- src/micro_app.ts
+++ src/micro_app.ts
@@ -13,8 +13,8 @@ const generateEnv = () => {
 const microApps = [
   {
     name: "micro_react",
-    entry: `//${generateEnv()}:9999/`,
-    activeRule: "/micro_app/micro_react/",
+    entry: `//${document.location.hostname}:9999/`,
+    activeRule: "/micro_react",
     container: "#subapp-viewport", // 子应用挂载的div
     props: {
       routerBase: "/micro_react",
